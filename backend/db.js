5045a364efb57a7e87baf0036c1eb34718820e2e
Sangeeta Jadoonanan <sjbitcode@gmail.com>
2019-02-02 17:31:00 -0500

    Perform collection create and data insert together

--- backend/db.js
+++ backend/db.js
@@ -8,34 +8,34 @@ const addDatabase = (databaseName, collectionName, url) => {
   return MongoClient.connect(`${url}`, { useNewUrlParser: true })
   .then(client => {
     const database = client.db(databaseName)
-    console.log(database)
+    // console.log(database)
     console.log('Connection successful!')
-    console.log(typeof (database))
-    console.log(Object.keys(database))
-    return database
-  })
-  .then(db => {
-    const collection = db.collection(collectionName)
-    console.log('Made a collection!')
-    return db
-  })
-  .then(db => {
+    // console.log(typeof (database))
+    // console.log(Object.keys(database))
+
+    const collection = database.collection(collectionName)
     const myobj = { name: "Company Inc", address: "Highway 37" }
-    db.collection(collectionName).insertOne(myobj)
-    console.log('Inserted stuff into the collection!')
-    return db
+    collection.insertOne(myobj)
+
+    return client
   })
-  .then(db => {
-    if(db) {
-      console.log('Closing db!')
-      console.log(typeof(db))
-      console.log(Object.keys(db))
-      // db.close()
-    }
+  // .then(db => {
+  //   const collection = db.collection(collectionName)
+  //   console.log('Made a collection!')
+  //   return db
+  // })
+  // .then(db => {
+  //   const myobj = { name: "Company Inc", address: "Highway 37" }
+  //   db.collection(collectionName).insertOne(myobj)
+  //   console.log('Inserted stuff into the collection!')
+  //   return db
+  // })
+  .then(client => {
+    client.close()
   })
   .catch((err) => {
-    console.log('wtf!')
-    // db.close()
+    console.log('some error!')
+    client.close()
     throw(err)
   })
 }
