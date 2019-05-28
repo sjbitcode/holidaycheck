7680ebc98bbf55b56b9bfb253f6c44bb29f49872
Sangeeta Jadoonanan <sjbitcode@gmail.com>
2019-05-28 15:23:58 -0400

    Remove user, task models. Remove weekday field from holiday model

--- backend/src/models/holiday.js
+++ backend/src/models/holiday.js
@@ -2,15 +2,11 @@ const mongoose = require('mongoose')
 const moment = require('moment')
 const aggregatePaginate = require('mongoose-aggregate-paginate-v2')
 
+
 const holidaySchema = new mongoose.Schema({
   date: {
     type: Date,
     required: true
-  },
-  weekday: {
-    type: String,
-    required: true,
-    trim: true
   },
   holidayName: {
     type: String,
@@ -32,22 +28,22 @@ const holidaySchema = new mongoose.Schema({
   }
 });
 
-// holidaySchema.index({ '$**': 'text' })
+// Helpful virtuals to get weekday and days duration.
 holidaySchema.virtual('calcWeekday').get(function() {
   const date = moment(this.date)
   return date._locale._weekdays[date.weekday()]
 })
-
 holidaySchema.virtual('daysFrom').get(function() {
   const date = moment(this.date)
   const today = moment().startOf('day')
   let days = date.diff(today, 'days')
   return days
 })
-
 holidaySchema.set('toJSON', { virtuals: true })
 holidaySchema.set('toObject', { virtuals: true, getters: true })
 
+
 holidaySchema.plugin(aggregatePaginate)
+
 
 module.exports = mongoose.model('Holiday', holidaySchema)
